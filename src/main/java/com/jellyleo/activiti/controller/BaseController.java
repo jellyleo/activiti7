f7d8986bcbe8f8ae7cad38cefd63bbb070542630
Jellyleo <jellyleo11@163.com>
2019-11-21 20:07:51 +0800

    formService替代方法

--- src/main/java/com/jellyleo/activiti/controller/BaseController.java
+++ src/main/java/com/jellyleo/activiti/controller/BaseController.java
@@ -1,9 +1,13 @@
 package com.jellyleo.activiti.controller;
 
+import java.util.List;
+
 import org.activiti.api.process.runtime.ProcessAdminRuntime;
 import org.activiti.api.process.runtime.ProcessRuntime;
 import org.activiti.api.task.runtime.TaskAdminRuntime;
 import org.activiti.api.task.runtime.TaskRuntime;
+import org.activiti.bpmn.model.FormProperty;
+import org.activiti.bpmn.model.UserTask;
 import org.activiti.engine.HistoryService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
@@ -13,7 +17,7 @@ import org.springframework.stereotype.Controller;
 
 /**
  * 
- * 功能描述:activiti控制器基类
+ * 功能描述:activiti基类
  *
  * @author Jelly
  * @created 2019年11月19日
@@ -22,37 +26,50 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class BaseController {
 	@Autowired
-	public TaskService taskService;
+	TaskService taskService;
 	@Autowired
-	public RuntimeService runtimeService;
+	RuntimeService runtimeService;
 	@Autowired
-	public HistoryService historyService;
+	HistoryService historyService;
 	@Autowired
-	public RepositoryService repositoryService;
+	RepositoryService repositoryService;
 
 	/**
 	 * ProcessRuntime类内部最终调用repositoryService和runtimeService相关API。 需要ACTIVITI_USER权限
 	 */
 	@Autowired
-	public ProcessRuntime processRuntime;
+	ProcessRuntime processRuntime;
 
 	/**
 	 * ProcessRuntime类内部最终调用repositoryService和runtimeService相关API。
 	 * 需要ACTIVITI_ADMIN权限
 	 */
 	@Autowired
-	public ProcessAdminRuntime processAdminRuntime;
+	ProcessAdminRuntime processAdminRuntime;
 
 	/**
 	 * 类内部调用taskService 需要ACTIVITI_USER权限
 	 */
 	@Autowired
-	public TaskRuntime taskRuntime;
+	TaskRuntime taskRuntime;
 
 	/**
 	 * 类内部调用taskService 需要ACTIVITI_ADMIN权限
 	 */
 	@Autowired
-	public TaskAdminRuntime taskAdminRuntime;
+	TaskAdminRuntime taskAdminRuntime;
 
+	/**
+	 * 
+	 * 功能描述:Acticiti7 formService替代方法
+	 *
+	 * @see [相关类/方法](可选)
+	 * @since [产品/模块版本](可选)
+	 */
+	List<FormProperty> getFormPropertiesOfOldFormService(String processDefinitionId, String taskDefinitionKey) {
+		// 在Acticiti7中，删除了FormService接口，可用以下方法代替
+		UserTask userTask = (UserTask) repositoryService.getBpmnModel(processDefinitionId)
+				.getFlowElement(taskDefinitionKey);
+		return userTask.getFormProperties();
+	}
 }
