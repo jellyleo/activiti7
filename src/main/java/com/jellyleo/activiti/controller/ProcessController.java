874be5ee2a56e5db83d55c9ef12edd11c0bcc5a9
Jellyleo <jellyleo11@163.com>
2019-11-21 11:32:08 +0800

    zip部署

--- src/main/java/com/jellyleo/activiti/controller/ProcessController.java
+++ src/main/java/com/jellyleo/activiti/controller/ProcessController.java
@@ -4,11 +4,16 @@
  */
 package com.jellyleo.activiti.controller;
 
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
+import java.util.zip.ZipInputStream;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
@@ -28,7 +33,7 @@ public class ProcessController extends BaseController {
 
 	/**
 	 * 
-	 * 功能描述:部署流程
+	 * 功能描述:classpath部署流程
 	 *
 	 * @param request
 	 * @param response
@@ -60,16 +65,102 @@ public class ProcessController extends BaseController {
 
 	/**
 	 * 
-	 * 功能描述:删除流程
+	 * 功能描述:zip部署流程
 	 *
 	 * @param request
 	 * @param response
 	 * @see [相关类/方法](可选)
 	 * @since [产品/模块版本](可选)
 	 */
-	@RequestMapping(value = "/deploy/delete")
+	@RequestMapping(value = "/deploy/zip")
 	@ResponseBody
-	public String delete(HttpServletRequest request, HttpServletResponse response) {
+	public String deployZip(HttpServletRequest request, HttpServletResponse response) {
+
+		String name = request.getParameter("name");
+		String zip = request.getParameter("zip");
+
+		if (StringUtils.isEmpty(zip) || StringUtils.isEmpty(name)) {
+			return "param error";
+		}
+
+		try {
+			InputStream in = this.getClass().getClassLoader().getResourceAsStream("processes/" + zip);
+			ZipInputStream zipInputStream = new ZipInputStream(in);
+			Deployment deployment = repositoryService// 与流程定义和部署对象相关的Service
+					.createDeployment()// 创建一个部署对象
+					.name(name)// 添加部署名称
+					.addZipInputStream(zipInputStream)// 完成zip文件的部署
+					.deploy();// 完成部署
+			System.out.println("部署ID：" + deployment.getId());
+			System.out.println("部署名称:" + deployment.getName());
+			System.out.println("*****************************************************************************");
+		} catch (Exception e) {
+			return "fail";
+		}
+		return "success";
+	}
+
+	/**
+	 * 
+	 * 功能描述:查询流程定义
+	 *
+	 * @param request
+	 * @param response
+	 * @see [相关类/方法](可选)
+	 * @since [产品/模块版本](可选)
+	 */
+	@RequestMapping(value = "/definition/query")
+	@ResponseBody
+	public String processDefinition(HttpServletRequest request, HttpServletResponse response) {
+
+		try {
+			List<ProcessDefinition> list = repositoryService// 与流程定义和部署对象相关的Service
+					.createProcessDefinitionQuery()// 创建一个流程定义查询
+					/* 指定查询条件,where条件 */
+					// .deploymentId(deploymentId)//使用部署对象ID查询
+					// .processDefinitionId(processDefinitionId)//使用流程定义ID查询
+					// .processDefinitionKey(processDefinitionKey)//使用流程定义的KEY查询
+					// .processDefinitionNameLike(processDefinitionNameLike)//使用流程定义的名称模糊查询
+
+					/* 排序 */
+					.orderByProcessDefinitionVersion().asc()// 按照版本的升序排列
+					// .orderByProcessDefinitionName().desc()//按照流程定义的名称降序排列
+
+					.list();// 返回一个集合列表，封装流程定义
+			// .singleResult();//返回唯一结果集
+			// .count();//返回结果集数量
+			// .listPage(firstResult, maxResults)//分页查询
+
+			if (list != null && list.size() > 0) {
+				for (ProcessDefinition processDefinition : list) {
+					System.out.println("流程定义ID:" + processDefinition.getId());// 流程定义的key+版本+随机生成数
+					System.out.println("流程定义名称:" + processDefinition.getName());// 对应HelloWorld.bpmn文件中的name属性值
+					System.out.println("流程定义的key:" + processDefinition.getKey());// 对应HelloWorld.bpmn文件中的id属性值
+					System.out.println("流程定义的版本:" + processDefinition.getVersion());// 当流程定义的key值相同的情况下，版本升级，默认从1开始
+					System.out.println("资源名称bpmn文件:" + processDefinition.getResourceName());
+					System.out.println("资源名称png文件:" + processDefinition.getDiagramResourceName());
+					System.out.println("部署对象ID:" + processDefinition.getDeploymentId());
+					System.out.println("*****************************************************************************");
+				}
+			}
+		} catch (Exception e) {
+			return "fail";
+		}
+		return "success";
+	}
+
+	/**
+	 * 
+	 * 功能描述:删除流程定义
+	 *
+	 * @param request
+	 * @param response
+	 * @see [相关类/方法](可选)
+	 * @since [产品/模块版本](可选)
+	 */
+	@RequestMapping(value = "/deployment/delete")
+	@ResponseBody
+	public String deleteDeployment(HttpServletRequest request, HttpServletResponse response) {
 
 		String deploymentId = request.getParameter("deployId");
 
@@ -78,8 +169,10 @@ public class ProcessController extends BaseController {
 		}
 
 		try {
-			repositoryService.deleteDeployment(deploymentId);
-
+//			// 不带级联的删除：只能删除没有启动的流程，如果流程启动，则抛出异常
+//			repositoryService.deleteDeployment(deploymentId);
+			// 能级联的删除：能删除启动的流程，会删除和当前规则相关的所有信息，正在执行的信息，也包括历史信息
+			repositoryService.deleteDeployment(deploymentId, true);
 			System.out.println("删除成功:" + deploymentId);
 			System.out.println("*****************************************************************************");
 		} catch (Exception e) {
@@ -145,7 +238,7 @@ public class ProcessController extends BaseController {
 		}
 
 		try {
-			runtimeService.deleteProcessInstance(processId, "结束");
+			runtimeService.deleteProcessInstance(processId, "流程已完毕");
 			System.out.println("终止流程");
 			System.out.println("*****************************************************************************");
 		} catch (Exception e) {
